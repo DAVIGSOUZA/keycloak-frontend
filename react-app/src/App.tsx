ee660b267952832339fecb4c14ec16f96b91b1da
Davi Garcia <davigs@hotmail.com>
2023-10-09 19:48:42 -0300

    remove react-app boilerplate

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div>
+      <h1>Area Logada</h1>
+
+      <h2>
+        React App
+      </h2>
+
+    
+      <h3>
+        Esta rota é privada, deve estar disponivel somente para usuário logado
+      </h3>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h4>
+          usuário logado:
+        </h4>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
