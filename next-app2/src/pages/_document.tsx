b5ec339b9c8e6f8df79687484f8a48ebafae0ee6
Davi Garcia <davigs@hotmail.com>
2024-05-21 09:37:57 -0300

    add mock request

--- next-app2/src/pages/_document.tsx
+++ next-app2/src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>TESTE</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
--- next-app2/src/pages/index.tsx
+++ next-app2/src/pages/index.tsx
@@ -1,24 +1,51 @@
 import { useKeycloak } from "@/context/keycloak"
+import axios from "axios";
+import { useState } from "react";
 
 export default function Home() {
+  const [fetching, setFetching] = useState(false)
+  const [backendData, setBackendData] = useState<any>(undefined)
+
   const {
     keycloak,
     isReady,
     loading,
   } = useKeycloak()
 
-  // console.log(keycloak?.idTokenParsed);
-  // console.log(keycloak?.authenticated);
   console.log({keycloak});
-  
+
+  const request = async () => {
+    setFetching(true)
+
+    const token = keycloak?.token
+
+    const bearerToken = `bearer ${token}`
+
+    const url = "SOME AUTH URL"
+
+    console.log('token used:', token);
+    
+    const response = await axios.get(
+      url, { headers: { Authorization: bearerToken}}
+    )
+    
+    setFetching(false)
+
+    setBackendData(response.data)
+  }
   
   return (
   <div>
     <h1> HOME </h1>
 
+    <br />
+
     <h2>
       Next app 2
     </h2>
+
+    <br />
+    <br />
 
     <div>
       {!isReady && loading && <h4> Loading ... </h4>}
@@ -28,6 +55,13 @@ export default function Home() {
       {isReady && !loading && keycloak?.authenticated && (
         <>
           <h4> Usuário logado</h4>
+
+          <h4> User Name: {keycloak?.idTokenParsed?.name}</h4>
+
+          <h4> User Email: {keycloak?.idTokenParsed?.email}</h4>
+
+          <br />
+
           <button onClick={() => keycloak.logout()}>Logout</button>
         </>
       )}
@@ -35,6 +69,27 @@ export default function Home() {
       {isReady && !loading && !(keycloak?.authenticated) && (
         <button onClick={() => keycloak?.login()}>Login</button>
       )}
+
+      <br />
+      <br />
+
+      <div>
+        {isReady && !loading && keycloak?.authenticated && (
+          <button onClick={() => request()}>Requisitar backend</button>
+        )}
+      </div>
+
+      <br />
+
+      <div>
+        {fetching && backendData === undefined && (
+          <h4> loading data from backend ... </h4>
+        )}
+
+        {!fetching && backendData !== undefined && (
+          <h4> {JSON.stringify(backendData)} </h4>
+        )}
+      </div>
     </div>
   </div>
   )
